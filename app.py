0090cee91cbe28f1bb8c9a6ef973dd5e4c32fe92
lydiasan <lyd.sanaa@gmail.com>
2020-05-13 15:58:23 +0200

    ajout du run.

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    subdomain = request.form['subdomain']
+    email = request.form['email']
+    password = request.form['password']
+    print(subdomain)
+    return render_template('request.py', subdomain=subdomain, email=email, password=password)
+
+@app.route('/db')
+def db():
+    return render_template('db.html')
+
+if __name__ == "__main__":
+    app.run()
--- request.py
+++ request.py
@@ -4,7 +4,10 @@ import pystaffo
 import requests
 import json
 import main
+from flask import Flask
 
+
+app = Flask(__name__)
 
 account = StaffoAccount(subdomain=subdomain, email=email, password=password)
 
@@ -13,3 +16,6 @@ account.get_location('Paris')
 # will return all shifts for specified location
 r = requests.get('https://api.github.com/user/locations/64/shifts.json', auth=('subdomain', 'email', 'password'))
 return r
+
+if __name__ == "__main__":
+app.run()
